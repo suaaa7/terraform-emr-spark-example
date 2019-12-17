52df731dd9f0659ba649b60f8f4ab900427a8be7
suaaa7 <spuaaa7@gmail.com>
2019-12-17 23:26:30 +0900

    Add required_providers

--- config.tf
+++ config.tf
@@ -1,32 +1,17 @@
 terraform {
   required_version = ">= 0.12.6"
+
+  required_providers {
+    archive  = "~> 1.3.0"
+    aws      = "~> 2.40.0"
+    http     = "~> 1.1.1"
+    local    = "~> 1.4.0"
+    random   = "~> 2.2.1"
+    template = "~> 2.1.2"
+    tls      = "~> 2.1.1"
+  }
 }
 
 provider "aws" {
-  region  = var.region
-  version = "~> 2.40.0"
-}
-
-provider "random" {
-  version = "~> 2.2.1"
-}
-
-provider "template" {
-  version = "~> 2.1.2"
-}
-
-provider "archive" {
-  version = "~> 1.3.0"
-}
-
-provider "tls" {
-  version = "~> 2.1.1"
-}
-
-provider "http" {
-  version = "~> 1.1.1"
-}
-
-provider "local" {
-  version = "~> 1.4.0"
+  region = var.region
 }

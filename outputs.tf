47d16f3f69b65506bf0d3db797720c9cd77f4d3b
suaaa7 <spuaaa7@gmail.com>
2019-12-29 18:19:43 +0900

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "dns_name" {
   value = module.lb.dns_name
 }
+
+output "master_dns_name" {
+  value = module.emr.master_dns_name
+}
+
+output "cluster_id" {
+  value = module.emr.cluster_id
+}

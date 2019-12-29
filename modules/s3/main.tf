31c6778de81cff8c00daf1a236b7aadfb43fa5a4
suaaa7 <spuaaa7@gmail.com>
2019-12-29 18:19:03 +0900

    Comment out s3_logs_bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,29 +1,29 @@
-resource "aws_s3_bucket" "s3_logs_bucket" {
-  bucket        = "${var.cluster_name}-${var.region}-${terraform.workspace}-s3logs"
-  acl           = "log-delivery-write"
-  force_destroy = true
+# resource "aws_s3_bucket" "s3_logs_bucket" {
+#   bucket        = "${var.cluster_name}-${var.region}-${terraform.workspace}-s3logs"
+#   acl           = "log-delivery-write"
+#   force_destroy = true
 
-  versioning {
-    enabled = true
-  }
+#   versioning {
+#     enabled = true
+#   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
+#   server_side_encryption_configuration {
+#     rule {
+#       apply_server_side_encryption_by_default {
+#         sse_algorithm = "AES256"
+#       }
+#     }
+#   }
+# }
 
 resource "aws_s3_bucket" "bucket" {
   bucket        = "${var.cluster_name}-${var.region}-${terraform.workspace}"
   acl           = "private"
   force_destroy = true
 
-  logging {
-    target_bucket = aws_s3_bucket.s3_logs_bucket.id
-  }
+  # logging {
+  #   target_bucket = aws_s3_bucket.s3_logs_bucket.id
+  # }
 
   versioning {
     enabled = true
